05a5925b3209965b6fd9c338de6c568032354fcc
Devendra Singh Chauhan <55349032+davreign-dav@users.noreply.github.com>
2023-04-07 20:10:55 +0530

    Update settings.py

--- disease_prediction/settings.py
+++ disease_prediction/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v3v5vfsn0xxjtmb=eoawoiw$5br4g0r&jy_l39995h_93l+-z5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 ALLOWED_HOSTS = ['*']
 
 
